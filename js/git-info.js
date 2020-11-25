63b64583b54fe5c28ace4d4afd7fb7b3d7588745
Vlad Egorov <33602561+jefoce@users.noreply.github.com>
2020-11-25 21:24:55 +0200

    ADD: Download link

---
+++ js/git-info.js
@@ -0,0 +1,43 @@
+const gitLink  = 'https://api.github.com/repos/jefoce/xmi/releases/latest';
+const isPath = (pathName) => window.location.pathname.includes(pathName);
+
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET', gitLink);
+xhr.onload = function() {
+
+	const git = JSON.parse(xhr?.response);
+	const info = {
+		ver : git?.tag_name ? ' ' + git?.tag_name : '',
+		link: git?.assets ? git?.assets[0]?.browser_download_url : '#'
+	}
+
+	onLoad(info);
+}
+xhr.send();
+
+
+/**
+ * @param {{ver: string; link: string}} info 
+ */
+function onLoad(info)
+{
+	if (isPath('download')) {
+		
+		let redirect = '404';
+
+		if (!!info.link)
+			redirect = info.link;
+
+		window.location.href = redirect;
+		setTimeout(() => window.location.href = '/', 5000);
+
+		return;
+	}
+
+
+	// Index Page
+	document.body.querySelector('#download').href = info.link;
+	document.body.querySelector('#version').textContent += info.ver;
+}
+
